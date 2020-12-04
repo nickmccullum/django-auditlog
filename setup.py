f1b3a6207ca273459940e858ed3126dac050e79c
Scott Stafford <scott.stafford@gmail.com>
2020-12-03 19:53:15 -0500

    updates to reqs for 0.4.5.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-auditlog',
-    version='0.4.5',
+    version='0.4.5.1',
     packages=['auditlog', 'auditlog.migrations', 'auditlog.management', 'auditlog.management.commands'],
     package_dir={'': 'src'},
     url='https://github.com/jjkester/django-auditlog',
@@ -11,8 +11,8 @@ setup(
     description='Audit log app for Django',
     install_requires=[
         'django-jsonfield>=1.0.0',
-        'django-jsonfield-compat==0.4.4',
-        'python-dateutil==2.6.0',
+        'django-jsonfield-compat',
+        'python-dateutil>=2.6.0',
     ],
     zip_safe=False
 )
