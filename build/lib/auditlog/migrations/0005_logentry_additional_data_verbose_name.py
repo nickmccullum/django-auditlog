d2f4ff082d69083621f3536239f7b84dd8135c15
Andrew Misiti <andrew.misiti@gmail.com>
2021-07-19 10:52:12 -0400

    django 3 support

---
+++ build/lib/auditlog/migrations/0005_logentry_additional_data_verbose_name.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+from jsonfield_compat import JSONField
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('auditlog', '0004_logentry_detailed_object_repr'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='logentry',
+            name='additional_data',
+            field=JSONField(null=True, verbose_name='additional data', blank=True),
+        ),
+    ]
