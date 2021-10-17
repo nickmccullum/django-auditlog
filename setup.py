8bc30e7561f261929a882dc7e67bcec728cc1551
Nick McCullum <nicholasmccullum@gmail.com>
2021-10-17 13:09:20 -0300

    changed requierments

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from distutils.core import setup
 
 setup(
     name='django-auditlog',
-    version='0.4.6',
+    version='0.4.5.2',
     packages=['auditlog', 'auditlog.migrations'],
     package_dir={'': 'src'},
     url='https://github.com/jjkester/django-auditlog',
     license='MIT',
     author='Jan-Jelle Kester',
     description='Audit log app for Django',
-    install_requires=[
-        'Django>=1.8',
-        'django-jsonfield>=1.0.0',
-        'django-jsonfield-compat==0.4.4',
-    ],
+    # install_requires=[
+    #     'Django>=1.8',
+    #     'django-jsonfield>=1.0.0',
+    #     'django-jsonfield-compat==0.4.4',
+    # ],
     zip_safe=False
 )
