126e0d92dedeac0affacc3be657dbe77c6a8fe3e
Nick McCullum <nicholasmccullum@gmail.com>
2021-10-17 12:59:28 -0300

    changed apps.py

--- src/auditlog/apps.py
+++ src/auditlog/apps.py
@@ -4,10 +4,10 @@ from django.apps import AppConfig
 
 
 class AuditlogConfig(AppConfig):
-    name = 'auditlog'
+    name = "auditlog"
     verbose_name = "Audit log"
 
-    def ready(self):
-        import jsonfield_compat
+    # def ready(self):
+    #     import jsonfield_compat
 
-        jsonfield_compat.register_app(self)
+    #     jsonfield_compat.register_app(self)
