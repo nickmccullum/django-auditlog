88e32348807d53607ed9c632bd9fb040155cf49c
Nick McCullum <nicholasmccullum@gmail.com>
2021-10-19 16:12:37 -0300

    removed jsonfield_compat reference

--- src/auditlog/migrations/0004_logentry_detailed_object_repr.py
+++ src/auditlog/migrations/0004_logentry_detailed_object_repr.py
@@ -3,19 +3,19 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 
-from jsonfield_compat import JSONField
+# from jsonfield_compat import JSONField
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('auditlog', '0003_logentry_remote_addr'),
+        ("auditlog", "0003_logentry_remote_addr"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='logentry',
-            name='additional_data',
-            field=JSONField(null=True, blank=True),
+            model_name="logentry",
+            name="additional_data",
+            field=models.JSONField(null=True, blank=True),
         ),
     ]
--- src/auditlog/migrations/0005_logentry_additional_data_verbose_name.py
+++ src/auditlog/migrations/0005_logentry_additional_data_verbose_name.py
@@ -3,19 +3,19 @@ from __future__ import unicode_literals
 
 from django.db import migrations, models
 
-from jsonfield_compat import JSONField
+# from jsonfield_compat import JSONField
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('auditlog', '0004_logentry_detailed_object_repr'),
+        ("auditlog", "0004_logentry_detailed_object_repr"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='logentry',
-            name='additional_data',
-            field=JSONField(null=True, verbose_name='additional data', blank=True),
+            model_name="logentry",
+            name="additional_data",
+            field=models.JSONField(null=True, verbose_name="additional data", blank=True),
         ),
     ]
